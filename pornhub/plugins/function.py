a2d0459e15d20f32514543ffa6b11b654c9a2c34
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-06-20 09:49:40 +0530

    Update function.py

--- pornhub/plugins/function.py
+++ pornhub/plugins/function.py
@@ -19,7 +19,7 @@ def humanbytes(size):
 
 def edit_msg(client, message, to_edit):
     try:
-       client.loop.create_task(asyncio.sleep(5)(message.edit(to_edit)))
+       client.loop.create_task(time.sleep(5)(message.edit(to_edit)))
     except FloodWait as e:
         client.loop.create_task(asyncio.sleep(e.value))
     except MessageNotModified:
@@ -37,5 +37,4 @@ def download_progress_hook(d, message, client):
         percent = d.get("_percent_str", "N/A")
         speed = d.get("_speed_str", "N/A")
         to_edit = f"📥 <b>Downloading!</b>\n\n<b>Name :</b> <code>{file_name}</code>\n<b>Size :</b> <code>{total}</code>\n<b>Speed :</b> <code>{speed}</code>\n<b>ETA :</b> <code>{eta}</code>\n\n<b>Percentage: </b> <code>{current}</code> from <code>{total} (__{percent}__)</code>"
-        time.sleep(3)
         threading.Thread(target=edit_msg, args=(client, message, to_edit)).start()
